1a1311d848c6906e8d539fc0cd77168969f4e3e6
Joshua Balmonte <jbalmonte45@gmail.com>
2021-08-25 17:37:03 +0800

    modified the color scheme of question component

--- src/components/Choice.jsx
+++ src/components/Choice.jsx
@@ -2,11 +2,11 @@
 const Choice = () => {
     return (
 
-        <div className="text-center flex mb-3">
-            <span className="flex items-center leading-normal rounded-l-lg border border-r-0 border-gray-300 px-3 whitespace-no-wrap text-secondary-100 ">
+        <div className="text-center flex">
+            <span className="flex items-center leading-normal rounded-l-md border border-r-0 border-gray-300 px-3 whitespace-no-wrap text-secondary-100 ">
                 <input type="radio" name="question1A" id="question1A" className="w-4 h-4" />
             </span>
-            <input type="text" placeholder="Input a choice" name="question1" id="question1" className="text-base px-4 py-2 border border-gray-300 rounded-r-lg focus:outline-none focus:border-green-400 w-11/12" />
+            <input type="text" name="question1" id="question1" className="text-base px-4 py-2 border border-gray-300 rounded-r-md focus:outline-none focus:border-green-400 w-11/12" />
         </div>
 
     )
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -4,11 +4,11 @@ import Choice from "./Choice"
 
 const Question = ({ itemNo }) => {
     return (
-        <div className="my-5">
-            <fieldset className=" text-gray-50 bg-secondary-200 w-3/4 mx-auto rounded-lg p-3">
-                <div className="text-center flex mt-2">
+        <div className="my-6">
+            <fieldset className=" text-primary border border-bg-gray-600 bg-gray-100 w-3/4 mx-auto rounded-lg p-3">
+                <div className="text-center flex mt-3">
                     <label htmlFor="question1" className="bg-green-500 text-gray-50 flex items-center leading-normal rounded-l-lg border border-r-0 border-green-500 px-3 whitespace-no-wrap ">Q{itemNo}</label>
-                    <input type="text" placeholder="Enter the question..." name="question1" id="question1" className="text-base px-4 py-2 border border-l-0 border-gray-300 rounded-r-lg focus:outline-none focus:border-green-400 w-11/12" />
+                    <input type="text" placeholder="Enter your question here" name=" question1" id="question1" className="text-base px-4 py-2 border border-l-0 border-gray-300 rounded-r-lg focus:outline-none focus:border-green-400 w-11/12" />
                 </div>
                 <div className="text-right my-3 mr-2">
                     <button type="button" className="text-right text-sm font-medium hover:text-green-600 hover:bg-gray-50 rounded-md">
@@ -21,12 +21,10 @@ const Question = ({ itemNo }) => {
                 <Choice />
                 <Choice />
                 <Choice />
-                <Choice />
 
-                <hr className="block" />
 
                 <div className="text-right mt-3">
-                    <span className="text-gray-200">Answer: </span>
+                    <span className="text-secondary-100">Answer: </span>
                     <span className="bg-tertiary text-gray-50 px-2 py-1 rounded-md ml-3 shadow text-center">Joshua</span>
                 </div>
             </fieldset>
